69bf1a64cf1009fa93715f426b815da875c5295a
hamzasx <hamzasxh@gmail.com>
2023-12-28 09:44:31 +0300

    Changed from Home.js to Main.js and add section Home.js, added images

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Home from "@/components/Home";
+import Main from "@/components/Main";
 import Header from "@/components/Header";
 import Image from "next/image";
 
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <>
       <Header />
-      <Home />
+      <Main />
     </>
   );
 }
--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,23 @@
-import Header from "./Header";
+import Image from "next/image";
+import mbImg from "../public/mobile/mb-img.png";
+import img from "../public/mobile/img.png";
 
 export default function Home() {
   return (
-    <section>
-      Home
-      <div></div>
+    <section id="home" className="home">
+      <h1 className="home__headline">Work at the speed of thought.</h1>
+      <p className="home__sub-headline">
+        Most calendars are designed for teams. Slate is designed for freelancers
+        who want a simple way to plan their schedule.
+      </p>
+
+      <div className="home__btn-container">
+        <button className="btn-1">Try For Free</button>
+        <button className="btn-2">Learn More</button>
+      </div>
+
+      <Image alt="" src={mbImg} width={2548} height={2048} />
+      {/* <Image alt="" src={img} width={3780} height={2048} /> */}
     </section>
   );
 }
